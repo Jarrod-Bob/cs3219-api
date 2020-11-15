a2cd6ea560ac783a6ee2269896a8d83f11e4a84f
Jarrod Ng <jarrodngjg@gmail.com>
2020-11-15 16:48:04 +0800

    Add mocha module and tests

---
+++ test/test.js
@@ -0,0 +1,105 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../index");
+
+chai.use(chaiHttp);
+chai.should();
+
+var students = [];
+
+describe("Students", () => {
+  describe("POST /students", () => {
+    it("should add a new student", (done) => {
+      chai
+        .request(app)
+        .post(`/students`)
+        .send({
+          name: "jonnycash",
+          email: "jc@jc.com",
+          faculty: "Science",
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.message.should.be.string("New student created!");
+          done();
+        });
+    });
+    it("should add a new student", (done) => {
+      chai
+        .request(app)
+        .post(`/students`)
+        .send({
+          name: "jonny",
+          email: "j@j.com",
+          faculty: "Science",
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.message.should.be.string("New student created!");
+          done();
+        });
+    });
+  });
+  describe("GET /", () => {
+    it("should get all students record", (done) => {
+      chai
+        .request(app)
+        .get("/students")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.message.should.be.string("Students retrieved successfully");
+          students = res.body.data;
+          done();
+        });
+    });
+    it("should get a single student at specified id", (done) => {
+      const id = students[0]._id;
+      chai
+        .request(app)
+        .get(`/students/${id}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.message.should.be.string("Student details loading..");
+          done();
+        });
+    });
+  });
+
+  describe("PUT /students/:id", () => {
+    it("should update a single student at specified id", (done) => {
+      const id = students[0]._id;
+      chai
+        .request(app)
+        .put(`/students/${id}`)
+        .send({
+          name: "jonnybravo",
+          email: "jb@jb.com",
+          faculty: "Business",
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.message.should.be.string("Student Info updated");
+          done();
+        });
+    });
+  });
+  describe("DELETE /students", () => {
+    it("should delete a single student at specified id", (done) => {
+      const id = students[0]._id;
+      chai
+        .request(app)
+        .del(`/students/${id}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.message.should.be.string("Student deleted");
+          done();
+        });
+    });
+  });
+});
